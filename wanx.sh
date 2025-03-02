21ce5c1caf20106a86f7505099cb382724e2c26a
UnrealComfy <unreal.xtr3me@gmail.com>
2025-03-02 03:29:21 +0100

    Update wanx.sh

--- wanx.sh
+++ wanx.sh
@@ -169,7 +169,7 @@ function provisioning_start() {
         "${WORKSPACE}/storage/stable_diffusion/models/esrgan" \
         "${ESRGAN_MODELS[@]}"
     provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/text_encoders" \
+        "/opt/ComfyUI/models/text_encoders" \
         "${TEXT_ENCODERS[@]}"
     provisioning_print_end
 }
