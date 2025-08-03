0d3bc3efe3bd35a40484fb91de1c4dc0d83175ee
UnrealComfy <unreal.xtr3me@gmail.com>
2025-08-03 21:20:27 +0200

    Update wan22.sh

--- wan22.sh
+++ wan22.sh
@@ -1,4 +1,4 @@
-cd ComfyUI/models/diffusion_models
+cd /ComfyUI/models/diffusion_models
 echo "Downloading Wan 22 FP8"
 wget --content-disposition "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors"
 wget --content-disposition "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
