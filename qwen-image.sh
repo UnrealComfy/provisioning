ab6be70acef87072815b41d24b69427af530b8d8
UnrealComfy <unreal.xtr3me@gmail.com>
2025-08-08 20:05:46 +0200

    Update qwen-image.sh

--- qwen-image.sh
+++ qwen-image.sh
@@ -4,6 +4,6 @@ wget --content-disposition "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/
 cd /ComfyUI/models/text_encoders
 echo "Downloading text encoder"
 wget --content-disposition "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors"
-cd cd /ComfyUI/models/vae
+cd /ComfyUI/models/vae
 echo "Downloading VAE"
 wget --content-disposition "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors"
