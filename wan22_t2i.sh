1387e628b32e00ea604f695a3ca3aebb5b35a2b7
UnrealComfy <unreal.xtr3me@gmail.com>
2025-08-09 13:20:58 +0200

    Create wan22_t2i.sh

---
+++ wan22_t2i.sh
@@ -0,0 +1,4 @@
+cd /ComfyUI/models/diffusion_models
+echo "Downloading Wan 22 T2V FP8"
+wget --content-disposition "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors"
+wget --content-disposition "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors"
